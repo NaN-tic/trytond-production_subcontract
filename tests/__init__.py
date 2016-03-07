09f0c410bb3efd38ffd4e4c191b9ee3ae3727ae0
Albert Cervera i Areny <albert@nan-tic.com>
2016-03-07 09:18:46 +0100

    Rename module from production_external to production_subcontract.
    Make basic workflow work. Missing parts:
    
    - Synchronize quantities between production and internal shipment
    - Improve internal shipment or change shipment type so multiple locations can
      be used in the same document.
    - Allow using produced quantities as supplier invoice quantity

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,3 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from .test_production_external import suite
+from .test_production_subcontract import suite
